7220752668033d2d9ad453af9df31c7544cd3e11
Tobias Andersson <tobias.ande@gmail.com>
2020-11-22 21:49:41 +0100

    catch exceptions

--- dn_prefix_getter.py
+++ dn_prefix_getter.py
@@ -1,36 +1,45 @@
 from prefix_getter import PrefixGetter
 import feedparser
 import random
+import time
 
 class DNPrefixGetter(PrefixGetter):
 	def __init__(self):
 		self.rss_feeds = [
-			#'http://expressen.se/rss/nyheter',
-			#'https://rss.aftonbladet.se/rss2/small/pages/sections/aftonbladet/',
-			#'http://api.sr.se/api/rss/program/83?format=145',
-			#'https://www.svt.se/nyheter/rss.xml',
+			'http://expressen.se/rss/nyheter',
+			'https://rss.aftonbladet.se/rss2/small/pages/sections/aftonbladet/',
+			'http://api.sr.se/api/rss/program/83?format=145',
+			'https://www.svt.se/nyheter/rss.xml',
 			'https://www.reddit.com/r/svenskpolitik/.rss',
 			'https://www.flashback.org/external.php?type=rss'
 			'https://www.reddit.com/r/sweden/.rss'
 		]
+		self.logger = logging.getLogger('Bot: ' + name)
+
 		self.index = 0
 		self.posts = []
 
 	def getPost(self):
 		source = random.choice(self.rss_feeds)
-		NewsFeed = feedparser.parse(source)
-		entry = random.choice(NewsFeed.entries)
+		try:
 
-		if len(self.posts) > 100:
-			self.posts = []
+			NewsFeed = feedparser.parse(source)
+			entry = random.choice(NewsFeed.entries)
 
-		if(entry['title'] in self.posts):
+			if len(self.posts) > 100:
+				self.posts = []
+
+			if(entry['title'] in self.posts):
+				return self.getPost()
+
+			self.posts.append(entry['title'])
+			return entry
+
+		except: # catch all
+			e = sys.exc_info()[0]
+			self.logger.error(e)
+			time.sleep(10)
 			return self.getPost()
-
-		self.posts.append(entry['title'])
-
-		return entry
-
 
 	def getPrefix(self, bot) -> str:
 		
