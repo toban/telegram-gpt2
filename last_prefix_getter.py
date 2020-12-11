0d4925dcc17d21d0f4b17300a270013178321d84
Tobias Andersson <tobias.ande@gmail.com>
2020-12-11 01:20:12 +0100

    reset session

--- last_prefix_getter.py
+++ last_prefix_getter.py
@@ -10,8 +10,12 @@ from io import BytesIO
 
 class LastPrefixGetter(PrefixGetter):
 
+	def __init__(self):
+		self.last_message = None
+
 	def getPrefix(self, bot, last_message) -> str:
-		if last_message is None:
+		if last_message is None or last_message == self.last_message:
 			return "hello"
-		else:
+		else: 
+			self.last_message = last_message
 			return last_message
--- text_generator.py
+++ text_generator.py
@@ -12,10 +12,11 @@ class TextGenerator:
 	def __init__(self):
 		self.logger = logging.getLogger('TextGenerator')
 		self.sess = gpt2.start_tf_sess()
-		gpt2.load_gpt2(self.sess)
 
 	def getMessages(self, prefix=None, length=1023):
 		self.logger.info("generating new texts ...")
+		self.sess = gpt2.reset_session(self.sess)
+		gpt2.load_gpt2(self.sess)
 		generated = gpt2.generate(self.sess, model_name="124M", return_as_list=True, prefix=prefix, length=length, temperature=1.0)
 		single_text = generated[0]
 		#f = open("demofile2.txt", "a")
